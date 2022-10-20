904dc033570c1733d1f62b0c80a81bc5a78d63a2
Diego.Cervantes <dcervantes8@ucol.mx>
2022-10-20 11:53:54 -0700

    modify app

--- py/app.py
+++ py/app.py
@@ -1,13 +1,16 @@
 from config import database as access
 from Models import models as model
 
-with access.app.app_context():
-    access.db.create_all();
-    print("creado con exito")
 
+##with access.app.app_context():
+  ##      access.db.create_all();
+    ##    print("creado con exito")
 def run():
-    pass
+    with access.app.app_context():
+        access.db.create_all();
+        print("creado con exito")
+
 
 if __name__ == '__main__':
    # access.app.run(debug=True)
-    run()
+     run()
