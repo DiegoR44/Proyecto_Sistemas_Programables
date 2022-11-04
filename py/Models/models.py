fcbda4b1d8f19bbfb2e000abbd77cee250af33cd
Diego.Cervantes <dcervantes8@ucol.mx>
2022-11-03 18:46:44 -0700

    module_new_register

--- py/Models/models.py
+++ py/Models/models.py
@@ -1,30 +1,33 @@
 from flask_login import UserMixin
 from utils.db import db
 
-from sqlalchemy import Column, Integer, BLOB,String
+from sqlalchemy import Column, Integer,BLOB,String
 
 class Cliente_huellas(UserMixin,db.Model):
 
     __tablename__ = 'Cliente_huella'
 
     id = Column(Integer, primary_key=True,autoincrement=True)
-    Huella= Column(String(100))
-    Cliente_pin=Column(Integer)
+    Huella= Column(String(100),unique=True)
+    Cliente_pin=Column(Integer,unique=True)
+    Nombre=Column(String(80))
     
-    def __init__(self, xHuella,xpincliente): ##constructor
-        self.Huella =xHuella
-        self.Cliente_pin=xpincliente
+    def __init__(self, Huella,Cliente_Pin,Nombre):##constructor
+        self.Huella =Huella
+        self.Cliente_pin=Cliente_Pin
+        self.Nombre=Nombre
 
     def __repr__(self):
-        return f'cliente_huella({self.Huella},{self.Cliente_pin})'
+        return f'cliente_huella({self.Huella},{self.Cliente_pin},{self.Nombre})'
 
     def __str__(self):
-        return self.Huella,self.Cliente_pin
+        return self.Huella,self.Cliente_pin,self.Nombre
 
     def serialize(self):
         return {
                 "Huella": self.Huella,
-                "Cliente pin": self.Cliente_pin}
+                "Cliente pin": self.Cliente_pin,
+                "Nombre": self.Nombre}
     
     
 
--- py/routes/resources.py
+++ py/routes/resources.py
@@ -1,35 +1,43 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify,flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from Models.models import Cliente_huellas
 from utils.db import db
 from flask_login import login_user
 
 recurso = Blueprint("resources", __name__)
-
-
 class Pines:
     @recurso.route("/")
     def home():
         return render_template("welcome.html")
 
+    @recurso.route("/to_register")
+    def to_register():
+        return render_template("registro.html")
+
     @recurso.route("/new_register", methods=["POST"])
     def new_register():
-        Huella = request.get_json(force=True)
-        Pin_cliente = request.get_json(force=True)
-
-        if Huella.get("Huella") and Pin_cliente.get("Cliente_pin") is None:
+        Huella = request.form.get('Huella')
+        Cliente_pin = request.form.get('Cliente_pin')
+        Nombre= request.form.get('Nombre')
+        
+        if Huella and Cliente_pin and Nombre  is None:
             return jsonify({"message": "Bad request"}), 400
+         
+        User_validation = Cliente_huellas.query.filter_by(Cliente_pin=Cliente_pin).first()
+        
+        if User_validation: # if a user is found, we want to redirect back to signup page so user can try again
+            flash('Email address already exists')
+            return render_template("welcome.html")
 
-        user = Cliente_huellas(
-           
-            Huella["Huella"], 
-            Pin_cliente["Cliente_pin"]
+        user_register = Cliente_huellas(
+            Huella=Huella, 
+            Cliente_pin=Cliente_pin,
+            Nombre=Nombre,
         )
-        db.session.add(user)
+        
+        db.session.add(user_register)
         db.session.commit()
         return "registro hecho"
-
-
 
     @recurso.route("/get_register", methods=["GET"])
     def get_register():
@@ -42,9 +50,9 @@ class Pines:
         Pin_cliente = request.get_json(force=True)
         
         user = Cliente_huellas.query.filter_by(Cliente_pin=Pin_cliente["Cliente_pin"]).first()
-     
+        
         if not user or not Huella["Huella"]:
-            print("revisa tus credenciales")
+          return "revisa tus credenciales"
             
         else:
            return render_template("welcome.html")
